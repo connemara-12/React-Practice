8a2870ea598bc111ea8e3b24e97eb61ef5ae5923
{connemara-12} <{denchi0123@yahoo.co.jp}>
2022-05-27 01:58:45 +0900

    ✏️  間違いの修正

--- Component/Header.js
+++ Component/Header.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import classes from './Header.module.css';
 
 export function Header() {
--- Component/Headline.js
+++ Component/Headline.js
@@ -10,4 +10,5 @@ export function Headline(props) {
         </p>
       </div>
   );
+  
 }
