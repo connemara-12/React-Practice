6de43e4a3336603eb747db565bec357a0bdc2ec2
{connemara-12} <{denchi0123@yahoo.co.jp}>
2022-05-27 23:44:32 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,24 +3,20 @@ import { Footer } from "src/Component/Footer";
 import { Header } from "src/Component/Header";
 import { Mian } from "src/Component/Main";
 import styles from "src/styles/Home.module.css";
-import { useCallback } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -31,9 +27,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      <h1>{count}</h1>
+      <button href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </button>
       <Mian page="index" />
       <Footer />
     </div>
