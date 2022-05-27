781354eb3e22742e5f6c6686f5457153c41c27c0
{connemara-12} <{denchi0123@yahoo.co.jp}>
2022-05-27 21:15:57 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,30 @@
-import Head from 'next/head'
-import { Footer } from 'src/Component/Footer'
-import { Header } from 'src/Component/Header'
-import { Mian } from 'src/Component/Main'
-import styles from 'src/styles/Home.module.css'
+import Head from "next/head";
+import { Footer } from "src/Component/Footer";
+import { Header } from "src/Component/Header";
+import { Mian } from "src/Component/Main";
+import styles from "src/styles/Home.module.css";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Mian page= "index"/>
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
+      <Mian page="index" />
       <Footer />
-      
     </div>
   );
 }
