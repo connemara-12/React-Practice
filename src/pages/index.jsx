311070528991595595ddf540c7173b7c8ccc47c3
{connemara-12} <{denchi0123@yahoo.co.jp}>
2022-05-27 22:21:27 +0900

    ✨ useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Header } from "src/Component/Header";
 import { Mian } from "src/Component/Main";
 import styles from "src/styles/Home.module.css";
 import { useCallback } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   const foo = 1;
@@ -12,6 +13,16 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
