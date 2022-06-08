4ceddac6bb06b5bf4277d23983d4195c55cf3e15
connemara-12 <ogawa0123.26@gmail.com>
2022-06-08 23:36:08 +0900

    ✨ 投稿の詳細ページにコメントを表示

---
+++ src/components/User/UserByUsertId.jsx
@@ -0,0 +1,18 @@
+import { fetcher } from "src/utils/fetcher";
+import useSWR from "swr";
+
+export const UserByUsertId = (props) => {
+  const { data, error } = useSWR(
+    props.id ? `https://jsonplaceholder.typicode.com/users/${props.id}` : null,
+    fetcher
+  );
+
+  if (!data && !error) {
+    return <div>ローディング中です</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Created by {data.name}</div>;
+};
